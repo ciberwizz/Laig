66a62dff4b21a7f3f120dfa56169ab72752d2ba3
Miguel Rossi Seabra <miguelrossis@gmail.com>
2012-09-26 11:07:06 +0100

    parsing tests

--- lsf_parser/src/parser.cpp
+++ lsf_parser/src/parser.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <cassert>
+#include "tinyxml.h"
 
 using namespace std;
 
 
 int main(){
-	cout << "heeelooo world!\n";
+	TiXmlDocument * doc;
+	doc=new TiXmlDocument( "lsf.lsf");
+	bool loadOkay = doc->LoadFile();
+
+	cout << "loadfile: "<< loadOkay;
+
+	TiXmlElement * lsfE = doc->FirstChildElement();
+	assert(lsfE != NULL);
+
+	TiXmlElement * globalsE = lsfE->FirstChildElement("globals");
+	assert(globalsE != NULL);
+
+	TiXmlElement * camerasE = lsfE->FirstChildElement("cameras");
+	assert(camerasE != NULL);
+
+	TiXmlElement * lightingE = lsfE->FirstChildElement("lighting");
+	assert(lightingE != NULL);
+
+	TiXmlElement * appearancesE = lsfE->FirstChildElement("appearances");
+	assert(appearancesE != NULL);
+
+	TiXmlElement * graphE = lsfE->FirstChildElement("graph");
+	assert(graphE != NULL);
 
 }
