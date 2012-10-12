e1826291a530d10ec7d81a19a8aaa7518cbf75eb
Miguel Rossi Seabra <ciberwizz@CiberHome.(none)>
2012-10-12 15:54:22 +0100

    LSFparser destructer and GraphNode implemented

--- lsf_parser/src/Graph.h
+++ lsf_parser/src/Graph.h
@@ -7,13 +7,15 @@
 
 #ifndef GRAPH_H_
 #define GRAPH_H_
+
+#include "GraphNode.h"
 #include "parser.h"
 #include <list>
 #include "cgf/CGFappearance.h"
 #include "cgf/CGFobject.h"
-/*
- *
- */
+
+
+
 class Graph
 {
 public:
@@ -22,18 +24,5 @@ public:
   virtual ~Graph(){};
 };
 
-class GraphNode {
-private:
-	CGFobject * obj;
-	CGFappearance * tex;
-	CGFappearance * material;
-	list<GraphNode *> children;
-
-public:
-	GraphNode(CGFobject * o, CGFappearance * _tex, CGFappearance * m)
-		{ obj = o; tex = _tex; material =m; this->children = list<GraphNode *>();};
-	void addChild( GraphNode * g) { this->children.push_back(g);};
-	//TODO funcoes....
-};
 #endif /* GRAPH_H_ */
 
